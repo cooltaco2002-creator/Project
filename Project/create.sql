f96835ee3fb07c58a1c0c490c8da5bde5d24a10b
Eric <cooltaco2002@gmail.com>
2025-11-27 15:01:37 -0700

    tables
    made ^

---
+++ Project/create.sql
@@ -0,0 +1,9 @@
+CREATE TABLE fishdata (
+id TEXT NOT NULL,
+common_name TEXT,
+scientific_name TEXT,
+average_length_cm INTERGER,
+habitat TEXT
+);
+
+
---
+++ Project/insert.sql
@@ -0,0 +1,6 @@
+INSERT INTO fishdata (id, common_name, scientific_name, average_length_cm, habitat) VALUES
+(1, 'Clownfish', 'Amphiprion ocellaris', 11, 'Tropical reefs'),
+(2, 'Atlantic Salmon', 'Salmo salar', 75, 'Freshwater & ocean'),
+(3, 'Blue Tang', 'Paracanthurus hepatus', 30, 'Coral reefs'),
+(4, 'Northern Pike', 'Esox lucius', 150, 'Freshwater lakes'),
+(5, 'Rainbow Trout', 'Oncorhynchus mykiss', 55, 'Cold freshwater streams');
